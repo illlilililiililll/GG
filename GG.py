5584a92f1bceb6cc571817d35562649cfae00a0f
illlilililiililll <kcwrenew22c@gmail.com>
2024-03-22 00:20:49 +0900

    GG

---
+++ GG.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import subprocess
+import data
+import util
+
+def GG(id, pw):
+    session = util.login(id, pw)
+    print('로그인되었습니다')
+
+    data = []
+    i = 1
+    while True:
+        response = session.get(f'https://hi.hana.hs.kr/SYSTEM_Plan/SubjectClassRoom/SCR_Application/applicationMain.asp?searchopt=&searchkey=&page={i}')
+        
+        if '개설된 모임이 없습니다.' in response.text:
+            break
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        for tr in soup.find_all('tr'):
+            row = [td.get_text(strip=True) for td in tr.find_all('td') if not td.find('input')]
+            if row and ('승인' in row):  # Exclude empty rows
+                row = [row[i] for i in [6, 4, 1, 7]]
+                row[0] = row[0][0]
+                row[3] = row[3][:-4]
+                data.append(row)
+        print(f'page #{i}')
+        i += 1
+
+    grouping_dict = defaultdict(lambda: defaultdict(list))
+    for item in data:
+        grouping_dict[item[0]][item[1]].append(item)
+
+    sorted_dict = dict(sorted(grouping_dict.items(), key=lambda x: (x[0], x[1].keys(), x[1].values())))
+
+    sorted_list = []
+    for main_key, sub_groups in sorted_dict.items():
+        sub_group_list = []
+        for sub_key, items in sorted(sub_groups.items(), key=lambda x: x[0]):
+            sub_group_list.append([sub_key, items])
+        sorted_list.append([main_key, sub_group_list])
+
+    return util.pretty_print(sorted_list)
+
+if __name__ == '__main__':
+    with open('GG.txt', 'w', encoding='utf-8') as f:
+        f.write(GG(data.id, data.pw))
+    print('파일이 저장되었습니다')
+    process = subprocess.Popen(['GG.txt'], shell=True)
---
+++ util.py
@@ -0,0 +1,34 @@
+import requests
+from random import randint
+
+def login(login_id, login_pw):
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    headers = {'User-Agent': user_agent,
+               'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
+    session = requests.Session()
+    session.headers.update(headers)
+
+    url_login_page = 'https://hi.hana.hs.kr/member/login.asp'
+    session.get(url_login_page)
+
+    url_login_proc = 'https://hi.hana.hs.kr/proc/login_proc.asp'
+    login_data = {'login_id': login_id, 'login_pw': login_pw, 'x': str(randint(10, 99)), 'y': str(randint(10, 99))}
+    res = session.post(url_login_proc, headers={'Referer': url_login_page}, data=login_data)
+    if '로그인 정보가' in res.text:
+        return
+    return session
+
+def pretty_print(data):
+    output_str = ""
+    for time_block in data:
+        time = time_block[0]
+        output_str += f"{time}타임\n"
+        for room_data in time_block[1]:
+            room = room_data[0]
+            output_str += f"\t{room}    {len(room_data[1])}명\n"
+            for student_data in room_data[1]:
+                student_name = student_data[3]
+                activity = student_data[2]
+                output_str += f"\t\t{student_name}, {activity}\n"
+            output_str += '\n'
+    return output_str.strip()
